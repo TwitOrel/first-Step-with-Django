8f5c715c06e890d7fabd572ebb06f5430512e560
TwitOrel <oreltwito3@gmail.com>
2025-03-08 15:08:16 -0800

    adding gitignore

--- testProject/settings.py
+++ testProject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,13 +34,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# For sending mails (forgot password~users/views.py "reset_password_request()")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'oreltwito3@gmail.com'
-EMAIL_HOST_PASSWORD = 'tekzyncuurbjehyu'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 
